75543cb6b96fb2f183f51de84623055bbc90b01e
AhmedElmeghalawy <52879089+AhmedElmeghalawy@users.noreply.github.com>
2023-01-27 18:34:23 +0200

    Update

--- server.js
+++ server.js
@@ -1,49 +1,53 @@
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 // Setup empty JS object to act as endpoint for all routes
-projectData = {
-
-};
+projectData = {};
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
+
+app.post("/post1", function (req, res) {
+  console.log("postData!");
+
+  projectData = {
+    zipCode: req.body.zipCode,
+    fealings: req.body.fealings,
+
+    unitType: req.body.unitType,
+    countryCode: req.body.countryCode,
+
+    cityName: req.body.cityName,
+    temp: req.body.temp,
+
+    date: req.body.date,
+  };
+  projectData.cityName ="123";
+  res.send(projectData).status(200);
+});
+
+app.get("/get", function (req, res) {
+  res.send(projectData).status(200);
+});
 
 // Setup Server
-let port = 8080;
-const server = app.listen(port, ()=>{listening()})
+let port = 80;
+const server = app.listen(port, () => {
+  listening();
+});
 
-function listening(){
-    console.log("server running"); 
-    console.log(`running on localhost: ${port}`);
+function listening() {
+  console.log("server running");
+  console.log(`running on localhost: ${port}`);
 }
-
-
-app.post('/postData', function (req, res) {
-  projectData={
-    temp:req.body.temp,
-    date:req.body.date,
-    content:req.body.content
-  };
-  res.send(projectData).status(200);
-})
-
-
-// app.get('/getData', function (req, res) {
-//   res.send(projectData).status(200);
-// })
-
-
 
 //https://review.udacity.com/#!/rubrics/4671/view
--- website/app.js
+++ website/app.js
@@ -1,129 +1,110 @@
-
 //API Key for OpenWeatherMap API
-const apiKey = '7161d1591e1007b8c2eacfb82da83f7a';
-
+const apiKey = "7161d1591e1007b8c2eacfb82da83f7a";
 
 let customUserDataObject = {
-
   //user input
-  zipCode: '',
-  fealings: '',
+  zipCode: "",
+  fealings: "",
 
-  unitType: '',
-  countryCode: '',
+  unitType: "",
+  countryCode: "",
 
-  //weather app data
-  cityName: '',
+  cityName: "",
+  temp: "",
 
-  temp: '',
-
-
-  //date dynamically with JS
-  date: ''
-
-}
-
+  date: "",
+};
 
 // Create a new date instance dynamically with JS
 
-document.getElementById('generate').onclick = function () {
+document.getElementById("generate").onclick = function () {
   console.log("generate btn clicked!");
 
-  getUserData();
-
-};
-
-document.getElementById('testing').onclick = function () {
-  console.log("testingData btn clicked!");
-
-  testingData();
-};
-
-function testingData() {
-  //used for debugging and testing only!...
-  console.log("Using Dummy data!");
-  document.getElementById('zip').value = '34510';
-  document.getElementById('feelings').value = 'I feel Awesome! 😎';
-  document.getElementById('unitType').value = 'imperial'; //imperial,metric
-  document.getElementById('countryCode').value = 'fr'; //us,fr,eg,uk, etc...
-}
-
-function getUserData() {
-  
-  //user input Data from the front-end/DOM elemnts value
-  customUserDataObject.zipCode = document.getElementById('zip');
-  customUserDataObject.fealings = document.getElementById('feelings');
-  customUserDataObject.unitType = document.getElementById('unitType');
-  customUserDataObject.countryCode = document.getElementById('countryCode');
-
-
-  //date assigned dynamicly using JS
-  const d = new Date();
-  customUserDataObject.newDate = d.getDate() + '.' + d.getMonth() + 1 + '.' + d.getFullYear();
-
-
+  getUserInputData();
 
   //Get weather data from "OpenWeatherApp" based on the inputs that was enterd by the user
   retrieveDataFromOpenWeatherApp();
+
+  //Sending the the custome data object (user Inputs data + weather data ) to (my)Server.
+  postDataToServer();
+};
+
+document.getElementById("testing").onclick = function () {
+  testingData();
+};
+//used for debugging and testing only!...
+function testingData() {
+  console.log("Using Dummy data!");
+  document.getElementById("zip").value = "34510";
+  document.getElementById("feelings").value = "I feel Awesome! 😎";
+  document.getElementById("unitType").value = "imperial"; //imperial,metric
+  document.getElementById("countryCode").value = "fr"; //us,fr,eg,uk, etc...
+}
+
+function getUserInputData() {
+  //user input Data from the front-end/DOM elemnts value
+  customUserDataObject.zipCode = document.getElementById("zip");
+  customUserDataObject.fealings = document.getElementById("feelings");
+  customUserDataObject.unitType = document.getElementById("unitType");
+  customUserDataObject.countryCode = document.getElementById("countryCode");
+
+  //date assigned dynamicly using JS
+  const d = new Date();
+  customUserDataObject.newDate =
+    d.getDate() + "." + d.getMonth() + 1 + "." + d.getFullYear();
 }
 
 //Get weather data from "OpenWeatherApp" based on the inputs that was enterd by the user
-//and using the developer(الي هو انا يعني😁) API key to get the data we want...
+//and using the developer's(الي هو انا يعني😁) API key to get the data we want...
 const retrieveDataFromOpenWeatherApp = async () => {
-
-  const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${customUserDataObject.zipCode.value},${customUserDataObject.countryCode.value}&appid=${apiKey}&units=${customUserDataObject.unitType}`);
+  const request = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?zip=${customUserDataObject.zipCode.value},${customUserDataObject.countryCode.value}&appid=${apiKey}&units=${customUserDataObject.unitType}`
+  );
   try {
-
     // Transform into JSON
-    const allData = await request.json()
-    console.log(allData)
+    const allData = await request.json();
+    console.log(allData);
 
-    customUserDataObject.cityName = allData.name
-
-
-    customUserDataObject.temp = allData.main.temp
-
-    customUserDataObject.Country = allData.sys.country
-
-
-    const logString =
-      `Country: ${customUserDataObject.Country} |
-    city name: ${customUserDataObject.cityName} |
-
-    temperature: ${Math.round(customUserDataObject.temp)} degrees |
-
-    Date: ${customUserDataObject.newDate} |
-
-    Feelings: ${customUserDataObject.fealings.value} |
-    `;
-
-    console.log(logString);
-
-    //Sending the the custome data object (user Inputs data + weather data ) to the Server.
-    PostDataToServer(customUserDataObject);
-
-
-    //Updating the UI
-    UpdateUI(logString);
-  }
-  catch (error) {
+    customUserDataObject.cityName = allData.name;
+    customUserDataObject.temp = allData.main.temp;
+    customUserDataObject.Country = allData.sys.country;
+  } catch (error) {
     console.log("error", error);
     // appropriately handle the error
   }
+};
 
+postDataToServer = async () => {
+  const request = await fetch(`http://127.0.0.1/post`);
+  try {
+    // Transform into JSON
+    const allData = await request.json();
+    console.log(allData);
 
-}
-function PostDataToServer(DataObj) {
-  //................................
-  //................................
-  //................................
-}
+    customUserDataObject.cityName = allData.name;
+    customUserDataObject.temp = allData.main.temp;
+    customUserDataObject.Country = allData.sys.country;
 
-function UpdateUI(Text) {
+    //Updating the UI
+    UpdateUI(customUserDataObject);
+
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+function UpdateUI(customUserDataObject) {
+  const logString = `
+    Country: ${customUserDataObject.Country} |
+    city name: ${customUserDataObject.cityName} |
+    temperature: ${Math.round(customUserDataObject.temp)} degrees |
+    Date: ${customUserDataObject.newDate} |
+    Feelings: ${customUserDataObject.fealings.value}
+    `;
 
   // Write updated data to DOM elements
-  document.getElementById('content').innerHTML = Text;
+  document.getElementById("content").innerHTML = logString;
+  console.log("UI Updated!");
 
 }
-
-
